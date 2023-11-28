a5708f88729d8b170958c23b376648d423e03dd1
wangyankun33 <2603109625@qq.com>
2023-11-28 14:04:05 +0800

    Add files via upload

---
+++ BindingAnimationEasingFunctions/README.md
@@ -0,0 +1,3 @@
+# BindingAnimationEasingFunctions
+引用包
+Xamarin.Android.Support.v7:28.0.0.3
---
+++ obj/Debug/generated/src/__NamespaceMapping__.cs
@@ -0,0 +1,14 @@
+using System;
+
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.daimajia.easing", Managed="Com.Daimajia.Easing")]
+
+#if !NET
+namespace System.Runtime.Versioning {
+    [System.Diagnostics.Conditional("NEVER")]
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Enum | AttributeTargets.Event | AttributeTargets.Field | AttributeTargets.Interface | AttributeTargets.Method | AttributeTargets.Module | AttributeTargets.Property | AttributeTargets.Struct, AllowMultiple = true, Inherited = false)]
+    internal sealed class SupportedOSPlatformAttribute : Attribute {
+        public SupportedOSPlatformAttribute (string platformName) { }
+    }
+}
+#endif
+
